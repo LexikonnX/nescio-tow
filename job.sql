91b38c50bde969fbc98e1f8942411f2ca03630cd
Lexikonn <108435506+LexikonnX@users.noreply.github.com>
2022-08-19 15:34:00 +0200

    Add files via upload

--- job.sql
+++ job.sql
@@ -1,2 +1,3 @@
+ALTER TABLE `owned_vehicles` ADD `impound` INT(1) NOT NULL DEFAULT '0' AFTER `type`, ADD `ngarage` VARCHAR(100) NULL DEFAULT NULL AFTER `impound`;
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES ('tow', 'Towing', '0');
 INSERT INTO `jobs` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES ('tow', '0', 'employee', 'Tow master', '30', '{}', '{}');
